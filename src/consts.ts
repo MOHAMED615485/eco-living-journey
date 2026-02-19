b7ef4390bcc9b7efa47db13af03a001f2bfbcc96
Mohamed Ben Hadj Djilali Megraoua <BENHADJDJILALIMEGRAOUAMOHAMED@GMAIL.COM>
2026-02-20 00:35:59 +0100

    Initial clean commit

---
+++ src/consts.ts
@@ -0,0 +1,5 @@
+// Place any global data in this file.
+// You can import this data from anywhere in your site by using the `import` keyword.
+
+export const SITE_TITLE = 'Eco Living Journey';
+export const SITE_DESCRIPTION = 'Sustainable Living for Apartments & Small Spaces';
