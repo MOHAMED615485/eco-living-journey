37430b3870a968920cd0ee371cff5f5a320ec114
MOHAMED615485 <BENHADJDJILALIMEGRAOUAMOHAMED@GMAIL.COM>
2026-04-16 21:56:20 +0100

    cleanup: remove python scripts from repo

--- fix_grow.py
+++
@@ -1,5 +0,0 @@
-content = open('src/layouts/BlogPost.astro', encoding='utf-8').read()
-old = '<script defer src="https://f.convertkit.com/ckjs/ck.5.js"></script>'
-new = old + '\n    <script async src="https://cdn.mediavine.com/grow/grow.js"></script>'
-open('src/layouts/BlogPost.astro', 'w', encoding='utf-8').write(content.replace(old, new))
-print('Done')
--- temp.py
+++
@@ -1,3 +0,0 @@
-from pathlib import Path
-
-content = open('C:/Users/DELL/Desktop/eco-living-journey/fix_faq.py', encoding='utf-8').read()
