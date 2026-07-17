3e24a7a5f5a6059081737e07eb359002c658f9b9
MOHAMED615485 <BENHADJDJILALIMEGRAOUAMOHAMED@GMAIL.COM>
2026-07-17 09:56:53 +0100

    security: remove unused Decap CMS admin panel and GitHub OAuth functions (repo-write attack surface)

--- functions/api/auth.js
+++
@@ -1,5 +0,0 @@
-export async function onRequestGet(context) {
-  const { GITHUB_CLIENT_ID } = context.env;
-  const redirectUrl = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=repo,user`;
-  return Response.redirect(redirectUrl, 302);
-}
